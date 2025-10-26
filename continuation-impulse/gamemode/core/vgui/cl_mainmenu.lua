5827868ced1e839f52e3fb5dc0d079aba9c5c9ce
Gordon Freeman <79755822+willmasterrr@users.noreply.github.com>
2025-10-26 10:25:18 +0100

    Update copyright information in cl_mainmenu.lua

--- continuation-impulse/gamemode/core/vgui/cl_mainmenu.lua
+++ continuation-impulse/gamemode/core/vgui/cl_mainmenu.lua
@@ -269,7 +269,7 @@ if not impulse.GetSetting("hud_original_hud") then
 		local year = os.date("%Y", os.time())
 		local copyrightLabel = vgui.Create("DLabel", self.core)
 		copyrightLabel:SetFont("Impulse-Elements14")
-		copyrightLabel:SetText("Powered by impulse\nCopyright 2i.games "..year.."\nimpulse version: "..impulse.Version.."\nPowered by Continuation Schema (By Mundee, https://github.com/willmasterLUA/continuation-hl2rp-public)")
+		copyrightLabel:SetText("Powered by impulse\nCopyright 2i.games "..year.."\nimpulse version: "..impulse.Version.."\nPowered by Continuation Schema (By willmasterr, https://github.com/willmasterrr/continuation-hl2rp-public)")
 		copyrightLabel:SizeToContents()
 		copyrightLabel:SetPos(ScrW()-copyrightLabel:GetWide(), ScrH()-copyrightLabel:GetTall()-5)
 	
@@ -623,7 +623,7 @@ else
 		local year = os.date("%Y", os.time())
 		local copyrightLabel = vgui.Create("DLabel", self.core)
 		copyrightLabel:SetFont("Impulse-Elements14")
-		copyrightLabel:SetText("Powered by impulse\nCopyright 2i.games "..year.."\nimpulse version: "..impulse.Version.."\nPowered by Continuation Schema (By Mundee, https://github.com/willmasterLUA/continuation-hl2rp-public)")
+		copyrightLabel:SetText("Powered by impulse\nCopyright 2i.games "..year.."\nimpulse version: "..impulse.Version.."\nPowered by Continuation Schema (By willmasterr, https://github.com/willmasterrr/continuation-hl2rp-public)")
 		copyrightLabel:SizeToContents()
 		copyrightLabel:SetPos(ScrW()-copyrightLabel:GetWide(), ScrH()-copyrightLabel:GetTall()-5)
 	
